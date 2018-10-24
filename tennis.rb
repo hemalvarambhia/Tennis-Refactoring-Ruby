81cff9eb6b6fc285dc38480ac13be9448f85228b
Hemal Varambhia <h.n.varambhia@gmail.com>
2018-10-24 22:17:25 +0100

    Broke up the turnary if-else to explicitly return from the function when it is all-square (15-15, 30-30).

--- tennis.rb
+++ tennis.rb
@@ -171,7 +171,8 @@ class TennisGame3
     if (@p1 < 4 and @p2 < 4) and (@p1 + @p2 < 6)
       p = ["Love", "Fifteen", "Thirty", "Forty"]
       s = p[@p1]
-      @p1 == @p2 ? s + "-All" : s + "-" + p[@p2]
+      return s + "-All" if @p1 == @p2
+      s + "-" + p[@p2]
     else
       s = @p1 > @p2 ? @p1N : @p2N
       (@p1-@p2)*(@p1-@p2) == 1 ? "Advantage " + s : "Win for " + s
