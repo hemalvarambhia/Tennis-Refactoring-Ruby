776aee0304be5ce7a0da75f9d277dab18af33e67
Hemal Varambhia <h.n.varambhia@gmail.com>
2018-10-27 16:57:16 +0100

    Split up the ternary if-else into guard clauses for winning and advantage.

--- tennis.rb
+++ tennis.rb
@@ -168,7 +168,8 @@ class TennisGame3
     
     if(@p1 > 3 || @p2 > 3)
       s = @p1 > @p2 ? @p1N : @p2N
-      return (@p1-@p2)*(@p1-@p2) == 1 ? "Advantage " + s : "Win for " + s
+      return "Advantage " + s if (@p1-@p2)*(@p1-@p2) == 1
+      return "Win for " + s if (@p1-@p2)*(@p1-@p2) > 1
     end
 
     "#{p[@p1]}-#{p[@p2]}"
